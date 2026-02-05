3a4233eed8a7fa8238bd0a508fbd073a80d9dbe1
heetdobariya-growexxer <heet.dobariya@growexx.com>
2026-02-05 16:48:27 +0530

    Complete payment processor implementation

---
+++ payment.js
@@ -0,0 +1,12 @@
+class PaymentProcessor {
+    constructor(gateway) {
+        this.gateway = gateway;
+    }
+
+    processPayment(amount) {
+        console.log(`Processing payment of ${amount}`);
+        return true;
+    }
+}
+
+module.exports = PaymentProcessor;
